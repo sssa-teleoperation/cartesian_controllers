d6e77a6457506ccd66a866d26da74e88024937a1
Tatiana <t.guerra@studenti.unipi.it>
2025-08-01 12:55:57 +0000

    pulizia

--- cartesian_motion_controller_test/cartesian_motion_controller_test/test_publisher_gradino.py
+++ cartesian_motion_controller_test/cartesian_motion_controller_test/test_publisher_gradino.py
@@ -24,10 +24,7 @@ class TestPublisher(Node):
         elapsed_time = time.time() - self.start_time 
 
         msg = Float64MultiArray()
-        amp = 0.06 # Ampiezza massima
-
-        #vx = 0.1 
-        # vx = amp * np.sin(2* np.pi * self.freq * elapsed_time)
+  
         if elapsed_time < 5.0: #gradino
             vz= 0.0
         elif elapsed_time < 10.0:
@@ -36,12 +33,7 @@ class TestPublisher(Node):
             vz = 0.0
 
         vy = vx = wx = wy = wz = 0.0  
-        # vy = amp * np.sin(2 * np.pi * self.freq * elapsed_time + np.pi/2)
-        # vz = amp * np.sin(2 * np.pi * self.freq * elapsed_time + np.pi)
-        # wx = 0.05 * np.sin(2 * np.pi * self.freq * elapsed_time)
-        # wy = 0.05 * np.sin(2 * np.pi * self.freq * elapsed_time + np.pi/2)
-        # wz = 0.05 * np.sin(2 * np.pi * self.freq * elapsed_time + np.pi)
-
+   
         msg.data = [vx, vy, vz, wx, wy, wz]
         self.publisher_.publish(msg)
 
@@ -53,7 +45,8 @@ def main():
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    # Invia comando di stop
+    
+    # Stop the controller by publishing zeros
     stop_msg = Float64MultiArray()
     stop_msg.data = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     node.publisher_.publish(stop_msg)
