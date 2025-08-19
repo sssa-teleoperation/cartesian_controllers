734b050de4f0af17f5be7163f20e2e9cc7507e10
Tatiana <t.guerra@studenti.unipi.it>
2025-08-19 16:27:36 +0000

    dati acquisiti con bag

--- cartesian_motion_controller_test/cartesian_motion_controller_test/actualvel_publisher2.py
+++ cartesian_motion_controller_test/cartesian_motion_controller_test/actualvel_publisher2.py
@@ -23,7 +23,7 @@ class ActualVelPublisher(Node):
             with open(csv_path, 'r') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    if row['phase_label'] == 'Release 54':
+                    if row['phase_label'] == 'Grasp 6':
                         self.data.append([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
                         break
                     else:
